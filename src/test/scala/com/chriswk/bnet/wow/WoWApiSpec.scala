7cebe355d13e88abf70114f5c5e358bbd5602285
chrkolst <christopher.kolstad@finn.no>
2013-08-02 13:48:06 +0200

    Make tests read better

--- src/test/scala/com/chriswk/bnet/wow/WoWApiSpec.scala
+++ src/test/scala/com/chriswk/bnet/wow/WoWApiSpec.scala
@@ -9,20 +9,19 @@ class WoWApiSpec extends Specification {
     "Use correct endpoint" in {
       api.guildQuery("test", "test").url === "http://eu.battle.net/api/wow/guild/test/test"
     }
-    "Auditor Fortuna Juvat is located at Aggramar (handles space)" in {
+    "handles space" in {
       api.guildQuery("Aggramar", "Auditor Fortuna Juvat").url === "http://eu.battle.net/api/wow/guild/Aggramar/Auditor%20Fortuna%20Juvat"
     }
   }
 
   "Url to character" should {
-    "Konichiwah is located at Aggramar" in {
+    "locate Konichiwah at Aggramar" in {
       api.charQuery("Aggramar", "Konichiwah").url === "http://eu.battle.net/api/wow/character/Aggramar/Konichiwah"
     }
 
-    "Should escape special characters correctly" in {
+    "escapes special characters correctly" in {
       api.charQuery("Aggramar", "Sónny").url === "http://eu.battle.net/api/wow/character/Aggramar/S%F3nny"
     }
   }
 
-  "Item"
 }
